61680ed413ab60d25dd55b8b740f65b564feed4f
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-10-09 11:36:15 +0200

    Tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-"""Import wait_random from 0-basic_async_syntax.
-
-Write a function (do not create an async function, use the regular
-function syntax to do this) task_wait_random that takes an integer
-max_delay and returns a asyncio.Task.
-"""
-
-
+"""Contains a method that spawns wait_random n times with a
+specified delay between each call."""
 import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
-    """Returns a asyncio.Task"""
-    return asyncio.create_task(wait_random(max_delay))
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with a specified delay
+    between each call.
+    Args:
+        n: number of times to spawn wait_random
+        max_delay: maximum delay between each call
+    Returns:
+        list of delays
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
+    

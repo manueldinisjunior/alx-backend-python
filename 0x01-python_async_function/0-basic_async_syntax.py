cb4aee936e8e9676b6b7a401194dd4e08581fc24
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-10-09 11:32:01 +0200

    Basic async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-"""Write an asynchronous coroutine to take in an integer argument named wait_random that waits
-for a random delay.
-Use the random module.
-"""
-
-import random
+'''Task 0's module.
+'''
 import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for some time"""
+    '''Waits for a random number of seconds.
+    '''
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
     return wait_time
-  
